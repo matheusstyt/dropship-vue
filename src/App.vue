8ea32ac941ff9f3a7019be5d3263542dd27ad87d
matheusstyt <matheus20galdino@gmail.com>
2023-01-12 17:51:51 -0400

    começando com a main

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,9 @@
   text-align: center;
   color: #2c3e50;
 }
+body{
 
+}
 nav {
   padding: 30px;
 
---
+++ src/components/Principal.vue
@@ -0,0 +1,195 @@
+<template>
+    <div>
+        <h1>Main</h1>
+        <main>
+            <div class="item">
+                <div class="photo">
+                    <!-- <img src="../assets/px200.png" alt=""> -->
+                </div>
+                <div class="flex-price">
+                    <h4>R$</h4><h2>36</h2><h4>,07</h4>
+                </div>
+                
+                <p>38 vendidos</p>
+                <P>Descrição básica do produto</P>
+            </div>
+            <div class="item">
+                <div class="photo">
+                    <!-- <img src="../assets/px200.png" alt=""> -->
+                </div>
+                <div class="flex-price">
+                    <h4>R$</h4><h2>36</h2><h4>,07</h4>
+                </div>
+                
+                <p>38 vendidos</p>
+                <P>Descrição básica do produto</P>
+            </div>
+            <div class="item">
+                <div class="photo">
+                    <!-- <img src="../assets/px200.png" alt=""> -->
+                </div>
+                <div class="flex-price">
+                    <h4>R$</h4><h2>36</h2><h4>,07</h4>
+                </div>
+                
+                <p>38 vendidos</p>
+                <P>Descrição básica do produto</P>
+            </div>
+            <div class="item">
+                <div class="photo">
+                    <!-- <img src="../assets/px200.png" alt=""> -->
+                </div>
+                <div class="flex-price">
+                    <h4>R$</h4><h2>36</h2><h4>,07</h4>
+                </div>
+                
+                <p>38 vendidos</p>
+                <P>Descrição básica do produto</P>
+            </div>
+            <div class="item">
+                <div class="photo">
+                    <!-- <img src="../assets/px200.png" alt=""> -->
+                </div>
+                <div class="flex-price">
+                    <h4>R$</h4><h2>36</h2><h4>,07</h4>
+                </div>
+                
+                <p>38 vendidos</p>
+                <P>Descrição básica do produto</P>
+            </div>
+            <div class="item">
+                <div class="photo">
+                    <!-- <img src="../assets/px200.png" alt=""> -->
+                </div>
+                <div class="flex-price">
+                    <h4>R$</h4><h2>36</h2><h4>,07</h4>
+                </div>
+                
+                <p>38 vendidos</p>
+                <P>Descrição básica do produto</P>
+            </div>
+            <div class="item">
+                <div class="photo">
+                    <!-- <img src="../assets/px200.png" alt=""> -->
+                </div>
+                <div class="flex-price">
+                    <h4>R$</h4><h2>36</h2><h4>,07</h4>
+                </div>
+                
+                <p>38 vendidos</p>
+                <P>Descrição básica do produto</P>
+            </div>
+            <div class="item">
+                <div class="photo">
+                    <!-- <img src="../assets/px200.png" alt=""> -->
+                </div>
+                <div class="flex-price">
+                    <h4>R$</h4><h2>36</h2><h4>,07</h4>
+                </div>
+                
+                <p>38 vendidos</p>
+                <P>Descrição básica do produto</P>
+            </div>
+            <div class="item">
+                <div class="photo">
+                    <!-- <img src="../assets/px200.png" alt=""> -->
+                </div>
+                <div class="flex-price">
+                    <h4>R$</h4><h2>36</h2><h4>,07</h4>
+                </div>
+                
+                <p>38 vendidos</p>
+                <P>Descrição básica do produto</P>
+            </div>
+            <div class="item">
+                <div class="photo">
+                    <!-- <img src="../assets/px200.png" alt=""> -->
+                </div>
+                <div class="flex-price">
+                    <h4>R$</h4><h2>36</h2><h4>,07</h4>
+                </div>
+                
+                <p>38 vendidos</p>
+                <P>Descrição básica do produto</P>
+            </div>
+            <div class="item">
+                <div class="photo">
+                    <!-- <img src="../assets/px200.png" alt=""> -->
+                </div>
+                <div class="flex-price">
+                    <h4>R$</h4><h2>36</h2><h4>,07</h4>
+                </div>
+                
+                <p>38 vendidos</p>
+                <P>Descrição básica do produto</P>
+            </div>
+            <div class="item">
+                <div class="photo">
+                    <!-- <img src="../assets/px200.png" alt=""> -->
+                </div>
+                <div class="flex-price">
+                    <h4>R$</h4><h2>36</h2><h4>,07</h4>
+                </div>
+                
+                <p>38 vendidos</p>
+                <P>Descrição básica do produto</P>
+                <div class="evaluation-content">
+                    <h4 id="evaluationIcon">&#9733;&#9733;&#9733;&#9733;&#9734;</h4>
+                    <p id="evaluationNumber"></p>
+                </div>
+            </div>
+
+        </main>
+    </div>
+</template>
+<style>
+h6, h4, h2, p{
+    color: rgb(65, 65, 65);
+    margin: 0;
+    padding: 0;
+}
+
+p{
+    text-align: left;
+    padding: 0.2em 0.5em;
+}
+main{
+    display: flex;
+    flex-flow: row wrap;
+    gap: 0.7vmax;
+    background-color: rgba(0,0,0, 0.0);
+    justify-content: center;
+    align-items: flex-start;
+    min-width: 17%;
+    max-width: 70%;
+    margin: 0 auto;
+}
+.item{
+    width: 230px;
+    height: 360px;
+    border-radius: 0.6em;
+    background-color: rgb(231, 231, 231);
+    overflow: hidden;
+}
+.item .photo{
+    width: 100%;
+    aspect-ratio: 1/1;
+    background-image: url(../assets/px200.png);
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: contain;
+    margin-bottom: 1em;
+}
+.flex-price{
+    padding: 0 0.5em;
+    display: flex;
+    justify-content:flex-start;
+    align-items: flex-end;
+}
+.item:hover{    
+    box-shadow: 0 0 10px rgba(0,0,0, 0.3);
+}
+.evaluation-content p{
+    text-align: center;
+}
+</style>
