32f2f52abc4095a370e0e3b1bb2731f8854e335d
matheusstyt <matheus20galdino@gmail.com>
2023-01-17 14:30:11 -0400

    finalizado a lógica das img's e detalhes css

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -3,13 +3,15 @@
         <Header/>
         <main class="container-product">
             <section class="img-product">
+
                 <img class="main-photo" :src="mainImg" alt="">
                 <ul class="list-photo">
-                    <li @mouseover="firstImg = true" @mouseleave="firstImg = false"><img :src="dataImg[0].src" alt=""></li>
-                    <li @mouseover="secondImg = true" @mouseleave="secondImg = false"><img :src="dataImg[1].src" alt=""></li>
-                    <li @mouseover="thirdImg = true" @mouseleave="thirdImg = false"><img :src="dataImg[2].src" alt=""></li>
-                    <li @mouseover="fourthImg = true" @mouseleave="fourthImg = false"><img :src="dataImg[3].src" alt=""></li>
-                    <li @mouseover="packImg = true" @mouseleave="packImg = false"><p>+3</p></li>
+                    <li @mouseover="firstImg = true" @mouseleave="firstImg = false"><img id="img1" crossorigin="anonymous" :src="dataImg[0].src" alt=""></li>
+                    <li @mouseover="secondImg = true" @mouseleave="secondImg = false"><img id="img2" crossorigin="anonymous" :src="dataImg[1].src" alt=""></li>
+                    <li @mouseover="thirdImg = true" @mouseleave="thirdImg = false"><img id="img3" crossorigin="anonymous" :src="dataImg[2].src" alt=""></li>
+                    <li @mouseover="fourthImg = true" @mouseleave="fourthImg = false"><img id="img4" crossorigin="anonymous" :src="dataImg[3].src" alt=""></li>
+               
+                    <li @mouseover="packImg = true" @mouseleave="packImg = false"><strong><span>+3</span></strong></li>
                     <!-- <li><img src="https://i.ebayimg.com/images/g/atkAAOSwMl1japLw/s-l1600.png" alt=""></li>
                     <li><img src="https://i.ebayimg.com/images/g/aBIAAOSwHEljapJ~/s-l1600.jpg" alt=""></li>
                     <li><img src="https://i.ebayimg.com/images/g/bgEAAOSwhCxjapKA/s-l1600.jpg" alt=""></li>
@@ -59,31 +61,58 @@ export default {
 
         }
     },
+    methods:{
+        changeOpacity(img){
+            if(img === 0){
+                document.getElementById('img1').style.opacity = 1;
+                document.getElementById('img2').style.opacity = 0.6;
+                document.getElementById('img3').style.opacity = 0.6;
+                document.getElementById('img4').style.opacity = 0.6;
+            }else if(img === 1){
+                document.getElementById('img1').style.opacity = 0.6;
+                document.getElementById('img2').style.opacity = 1;
+                document.getElementById('img3').style.opacity = 0.6;
+                document.getElementById('img4').style.opacity = 0.6;
+            }else if(img === 2){
+                document.getElementById('img1').style.opacity = 0.6;
+                document.getElementById('img2').style.opacity = 0.6;
+                document.getElementById('img3').style.opacity = 1;
+                document.getElementById('img4').style.opacity = 0.6;
+            }else if(img === 3){
+                document.getElementById('img1').style.opacity = 0.6;
+                document.getElementById('img2').style.opacity = 0.6;
+                document.getElementById('img3').style.opacity = 0.6;
+                document.getElementById('img4').style.opacity = 1;
+            }
+        }
+    },
     watch:{
         firstImg(newValue){ 
             if(newValue){
-                this.mainImg = this.dataImg[1].src
+                this.mainImg = this.dataImg[0].src
+                this.changeOpacity(0);
             }
-            console.log('fisrt'+newValue); 
     },
         secondImg(newValue){ 
             if(newValue){
-                this.mainImg = this.dataImg[2].src
+                this.mainImg = this.dataImg[1].src
+                this.changeOpacity(1);
             }
-            console.log('second'+newValue); 
     },
         thirdImg(newValue){ 
             if(newValue){
-                this.mainImg = this.dataImg[3].src
+                this.mainImg = this.dataImg[2].src
+                this.changeOpacity(2);
             }
-            console.log('third'+newValue); 
     },
         fourthImg(newValue){ 
-            
-            console.log('fourth'+newValue); 
+            if(newValue){
+                this.mainImg = this.dataImg[3].src
+                this.changeOpacity(3);
+            }
     },
         packImg(newValue){ 
-            
+
             console.log('pack'+newValue); 
     },
     },
@@ -96,46 +125,91 @@ export default {
 </script>
 <style lang="scss" scoped>
 main{
+    
     max-width: 100%;
-    width: 100%;
+    background-color: aqua;
+    
 }
 .container-product{
     display: flex;
-    background-color: bisque;
-    width: 100%;
+    background-color: white;
+    width: 97%;
+    margin: 0 auto;
 }
 .img-product{
     display: flex;
     flex-flow: row wrap;
+    gap: 1em;
     width: 45%;
-    background-color: rgba(0, 0, 0,0.3);
+
+    aspect-ratio: 4/3;
+   // background-color: rgba(0, 0, 0,0.3);
 }
 .details-product{
-    width: 55%;
-    background-color: rgba(0, 0, 0,0.5);
+    width: 50%;
+    //background-color: rgba(0, 0, 0,0.5);
 }
 .main-photo{
-    width: 75%;
-    aspect-ratio: 4/3;
+    width: 82%;
+    aspect-ratio: 1/1;
+    max-height: 100%;
+    cursor: pointer;
+    border: 1px solid rgba(0, 0, 0, 0.3);
+    overflow: hidden;
 }
 .list-photo{
-    width: 10%;
+    width: 14%;
+    margin: 0;
+    padding: 0;
     list-style-type: none;
-
+    display: flex;
+    flex-flow: row wrap;
+    gap: 1em;
+    //overflow-y: scroll;
+    height: 100%;
+    border-bottom: 5px solid hsla(0,0%,100%,.5);
     li{
         overflow: hidden;
+        aspect-ratio: 1/1;
+    }
+    li:first-child{
+        background-color: transparent;
     }
     li img{
-        width: 100%;
+        width: 90%;
         aspect-ratio: 1/1;
+        opacity: 0.7;
+        //cursor: pointer;
+        //border: 1px solid rgba(0, 0, 0, 0.3);
+        //overflow: hidden;
     }
-    li p{
-        width: 100%;
+    li img:hover{
+        transition: ease 0.2s;
+        opacity: 1;
+        .main-photo{
+            src: v-bind('mainImg[2].src');
+        }
+    }
+    li{
+        width: 90%;
         aspect-ratio: 1/1;
-        background-color: aliceblue;
         display: flex;
         align-items: center;
         justify-content: center;
+        cursor: pointer;
+        border: 1px solid rgba(0, 0, 0, 0.3);
+    }
+    li strong{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+        height: 100%;
+    }
+    li strong:hover{
+        transition: ease 0.2s;
+        background-color: rgba(0, 0, 0, 0.1);
+        
     }
 }
 </style>
